aa6cae3ea1b40e047311113d51dd4a09f322594d
JaydenHD <65391259+JaydenHD@users.noreply.github.com>
2021-08-15 01:28:44 +0800

    Update PAT-B1023.组个最小数.cpp

--- PAT/cSrc/PAT-B1023.组个最小数.cpp
+++ PAT/cSrc/PAT-B1023.组个最小数.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
 using namespace std;
-int main() {
+int main() 
+{
     int n[10];/* 申明一个包含10个整数的数组n */
+    
+    /* 输入10个整数 */	
+    for (int i = 0; i < 10; i++) 
+    {
+	 cin >> n[i]; 
+    }
 
-    for(int i=0;i<10;i++) {/* 输入10个整数 */
-		cin>>n[i]; 
-    	}
-	
-    for(int i=1;i<10;i++) {/* 先输出最小的非零整数 */
-       
-	 if(n[i]>0) {  /* 判断最小的非零整数 */
-            cout<<i;
-            n[i]--; /* 数组减一 */
+    /* 先输出最小的非零整数 */	
+    for (int i = 1; i < 10; i++)
+    { 
+	 /* 判断最小的非零整数 */   
+	 if (n[i] > 0) {  
+            cout << i;
+            n[i]--; /* 该位置对应的元素 -1 */
             break;
-	    }
-	}
+	 }
+     }
 	
-    for(int i=0;i<10;i++) {/* 依次从小到大输出剩下的数 */ 
-        for(int j=0;j<n[i];j++) { 
-            cout<<i;
-	    }
-	return 0;
-       }
+    /* 依次从小到大输出剩下的数 */	
+    for (int i = 0; i < 10; i++) 
+    { 
+        for (int j = 0; j < n[i]; j++) 
+	{ 
+            cout << i;
+	}
+    }
+    return 0;      
 }
