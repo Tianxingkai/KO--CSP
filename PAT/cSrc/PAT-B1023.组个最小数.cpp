7e59b87904880b59824c3882a24082c42e88bec6
jaydenhd <594075381@qq.com>
2021-08-14 16:13:39 +0800

    [Solution]PAT-B1023.组个最小数 C++ 解法(#209)

---
+++ PAT/cSrc/PAT-B1023.组个最小数.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n[10];/* 申明一个包含10个整数的数组n */
+
+    for(int i=0;i<10;i++)/* 输入10个整数 */
+		cin>>n[i]; 
+	
+    for(int i=1;i<10;i++)/* 先输出最小的非零整数 */
+        if(n[i]>0){  /* 判断最小的非零整数 */
+            cout<<i;
+            n[i]--; /* 数组减一 */
+            break;
+		}
+	
+    for(int i=0;i<10;i++)/* 依次从小到大输出剩下的数 */ 
+        for(int j=0;j<n[i];j++) 
+            cout<<i;
+	return 0;
+}
