1cf68896152518f0f4cc93cdecb24e94ca5e0c34
天行凯 <83781603+Tianxingkai@users.noreply.github.com>
2021-08-21 14:45:06 +0800

    Update PAT-B1001.害死人不偿命的(3n+1)猜想.java
    修改错误格式，

--- PAT/javaSrc/PAT-B1001.害死人不偿命的(3n+1)猜想.java
+++ PAT/javaSrc/PAT-B1001.害死人不偿命的(3n+1)猜想.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
-public class B1001 {
+public class Main {
     public static void main(String[] args) {
         int step = 0;// 步长
-        System.out.println("请输入一个自然数：");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         while (n != 1) {
@@ -14,6 +13,6 @@ public class B1001 {
             }
             step++;
         }
-        System.out.println("step = " + step);
+        System.out.println(step);
     }
 }
